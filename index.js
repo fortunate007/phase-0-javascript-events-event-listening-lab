2fbd03780dee9ca3baace10fd4efce7965029f7f
Newton <newtonkirimi55@gmail.com>
2024-07-06 07:01:36 -0700

    JavaScript Event Listeners Lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
 }
+function addingEventListener() {
+    const input = document.getElementById('button'); // Replace 'button' with the actual ID of your element
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+        // Additional code to handle the click event
+    });
+}
+
+// Call addingEventListener to bind the event listener
+addingEventListener();
